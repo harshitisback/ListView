fe0314a8e3de4b2a1e0afc2a0b7d2b04e711fc1c
Aman <harshit.tiwari2020@vitbhopal.ac.in>
2022-06-25 08:44:53 +0530

    adapter added

--- app/src/main/java/com/example/listview/CustomPojo.java
+++ app/src/main/java/com/example/listview/CustomPojo.java
@@ -1,4 +1,33 @@
 package com.example.listview;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
 public class CustomPojo {
+    int images;
+    String langName;
+
+    public CustomPojo(int images, String langName) {
+        this.images = images;
+        this.langName = langName;
+    }
+
+    public int getImages() {
+        return images;
+    }
+
+    public void setImages(int images) {
+        this.images = images;
+    }
+
+    public String getLangName() {
+        return langName;
+    }
+
+    public void setLangName(String langName) {
+        this.langName = langName;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/listview/adapterData.java
@@ -0,0 +1,34 @@
+package com.example.listview;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class adapterData extends BaseAdapter {
+
+    ArrayList<CustomPojo> cp = new ArrayList<>();
+    private List<CustomPojo> listPojo;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
